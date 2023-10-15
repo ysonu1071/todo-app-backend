3e8a36c4bf72f97da0b8ccfb6e72c685b0620e6e
ysonu1071 <ysonu1071@gmail.com>
2023-10-15 18:31:24 +0530

    adding forget password functionality

--- controller/userController.js
+++ controller/userController.js
@@ -1,56 +1,107 @@
-const User = require("../models/userModel");
+
 const bcrypt = require("bcrypt");
 const JWT = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const UserModel = require("../models/userModel");
 
-let SECRATE_KEY = "sonu1234";
 
-const registerUser = async(req, res) => {
+const registerUser = async (req, res) => {
 
-    const {name, email, password} = req.body;
+    const { name, email, password } = req.body;
 
     try {
-        let user = await User.find({email});
+        let user = await UserModel.find({ email });
 
-        if(user.length != 0){
-            return res.status(200).json({status: "fail", message:"User allready exist!"})
+        if (user.length != 0) {
+            return res.status(200).json({ status: "fail", message: "User allready exist!" })
         }
 
         let hashedPassword = await bcrypt.hash(password, 10);
-        user = await User.create({name, email, password: hashedPassword});
-        return res.status(200).json({status:"success", message:"User registered successfully"});
-        
+        user = await UserModel.create({ name, email, password: hashedPassword });
+        return res.status(200).json({ status: "success", message: "User registered successfully" });
+
     } catch (error) {
-        return res.status(500).json({status:"fail", message: error.message});
+        return res.status(500).json({ status: "fail", message: error.message });
     }
 
 }
 
-const loginUser = async(req, res) => {
+const loginUser = async (req, res) => {
 
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
     try {
-        let user = await User.find({email});
-        if(user.length == 0){
-            return res.status(200).json({status: 'fail', message: 'User not found!'});
-        }
-        
-        let isPasswordCorrect = await bcrypt.compare(password, user[0].password);
-        if(!isPasswordCorrect){
-            return res.status(200).json({status:"fail", message:"Incorrect password"});
+        let user = await UserModel.find({ email });
+        if (user.length == 0) {
+            return res.status(200).json({ status: 'fail', message: 'User not found!' });
         }
 
-        let token = JWT.sign({name: user[0].name, email: user[0].email, id: user[0]._id}, SECRATE_KEY);
+        let isPasswordCorrect = await bcrypt.compare(password, user[0].password);
+        if (!isPasswordCorrect) {
+            return res.status(200).json({ status: "fail", message: "Incorrect password" });
+        }
+
+        let token = JWT.sign({ name: user[0].name, email: user[0].email, id: user[0]._id }, process.env["SECRATE_KEY"]);
 
         // res.cookie("token", token, {httpOnly: true, maxAge: 86400000});
 
-        return res.status(200).json({status:"success", message:"Login successfull", data:{name: user[0].name}, token: token});
+        return res.status(200).json({ status: "success", message: "Login successfull", data: { name: user[0].name }, token: token });
 
     } catch (error) {
         console.log(error.message);
-        return res.status(500).json({status: "fail", message: error.message});
+        return res.status(500).json({ status: "fail", message: error.message });
     }
 }
 
 
-module.exports = {registerUser, loginUser};
+const sendOtpForForgetPassword = async (req, res) => {
+    const { email, otp } = req.body;
+    try {
+        let user = await UserModel.find({ email });
+        if (user.length == 0) {
+            return res.status(200).json({ status: 'fail', message: 'User not found!' });
+        }
+        // Create a transporter 
+        let transporter = nodemailer.createTransport({
+            host: "smtp-relay.sendinblue.com",
+            port: 587,
+            auth: {
+                user: process.env.USER,
+                pass: process.env.PASS,
+            },
+        });
+
+
+        // send mail with defined transport object 
+        let info = await transporter.sendMail({
+            from: process.env.USER,// sender address 
+            to: email,// list of receivers 
+            subject: "Todo app otp for generating new password",// subject line 
+            text: `This is the otp  ${otp}`,// plain text body 
+        });
+
+        return res.status(200).json({ status: "success", message: "Otp send successfully" });
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ status: "fail", message: error.message });
+    }
+}
+
+
+const saveNewPassword = async (req, res) => {
+    const { email, newPassword } = req.body;
+
+    try {
+        let hashedPassword = await bcrypt.hash(newPassword, 10);
+        let updatedData = await UserModel.findOneAndUpdate({ email }, { password: hashedPassword })
+
+        return res.status(201).json({ status: "success", message: "Password updated successfully" });
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ status: "fail", message: error.message });
+    }
+
+}
+
+
+module.exports = { registerUser, loginUser, sendOtpForForgetPassword, saveNewPassword };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { registerUser, loginUser} = require("../controller/userController");
+const { registerUser, loginUser, sendOtpForForgetPassword, saveNewPassword} = require("../controller/userController");
 
 const route = express.Router();
 
 route.post("/register", registerUser);
 route.post("/login", loginUser);
+route.post("/sendotp", sendOtpForForgetPassword);
+route.post("/savenewpassword", saveNewPassword);
 
 
 
